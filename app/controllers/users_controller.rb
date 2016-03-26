3ab640621055dbc2f321412407e70541621251bf
rachelswansen <rachel.swansen@gmail.com>
2016-03-26 23:48:53 +0000

    Did a tutorial to set up authentication. Still need to test it.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController  
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :confirmation)
+  end
+  def index
+    @users = User.all()
+  end
   def new
+    @user = User.new 
+  end
+  def create
+    @user = User.new(user_params)
+      flash[:notice] = "You signed up successfully"
+      flash[:color]= "valid"
+
+    render "index"
   end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
+
 class User < ActiveRecord::Base
+  attr_accessor :password, :username, :email
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
+  validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
+  validates :password, :confirmation => true #password_confirmation attr
+  validates_length_of :password, :in => 6..20, :on => :create
 end
