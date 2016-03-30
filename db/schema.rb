ce86f411f9ef1ac6a0a2e3b2c6000e12b9bbd86f
travisbritz <travis.britz@mnsu.edu>
2016-03-30 17:51:04 +0000

    created migration for core db tables

---
+++ db/schema.rb
@@ -0,0 +1,63 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160330164350) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "fname"
+    t.string "lname"
+  end
+
+  create_table "authors_books", id: false, force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.integer "book_id",   null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string  "isbn"
+    t.string  "title"
+    t.text    "description"
+    t.binary  "thumbnail"
+    t.integer "publisher_id"
+  end
+
+  create_table "publishers", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "zipcode"
+  end
+
+  create_table "sell_listings", force: :cascade do |t|
+    t.decimal  "price"
+    t.integer  "condition"
+    t.text     "notes"
+    t.integer  "book_id"
+    t.boolean  "is_hardcover"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.boolean  "email_verified"
+    t.boolean  "is_admin"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
