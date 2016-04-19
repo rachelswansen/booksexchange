45df734bb1b9dbd6c5cf96d04327a43980cb07ef
sol123 <solomon.woldegebriel@mnsu.edu>
2016-04-19 18:37:33 +0000

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160415002132) do
-
-  create_table "listings", force: :cascade do |t|
-    t.string   "title"
-    t.string   "isbn"
-    t.decimal  "price"
-    t.text     "description"
-    t.boolean  "is_hardcover"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.string   "thumbnail_file_name"
-    t.string   "thumbnail_content_type"
-    t.integer  "thumbnail_file_size"
-    t.datetime "thumbnail_updated_at"
-=======
-ActiveRecord::Schema.define(version: 20160330164350) do
 
   create_table "authors", force: :cascade do |t|
     t.string "fname"
@@ -45,6 +29,20 @@ ActiveRecord::Schema.define(version: 20160330164350) do
     t.text    "description"
     t.binary  "thumbnail"
     t.integer "publisher_id"
+  end
+
+  create_table "listings", force: :cascade do |t|
+    t.string   "title"
+    t.string   "isbn"
+    t.decimal  "price"
+    t.text     "description"
+    t.boolean  "is_hardcover"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.string   "thumbnail_file_name"
+    t.string   "thumbnail_content_type"
+    t.integer  "thumbnail_file_size"
+    t.datetime "thumbnail_updated_at"
   end
 
   create_table "publishers", force: :cascade do |t|
@@ -74,7 +72,6 @@ ActiveRecord::Schema.define(version: 20160330164350) do
     t.boolean  "is_admin"
     t.datetime "created_at"
     t.datetime "updated_at"
->>>>>>> master
   end
 
 end
