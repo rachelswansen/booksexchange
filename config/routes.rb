752c6c4ee682318c401811709b7f488f00d29dc0
travisbritz <travis.britz@mnsu.edu>
2016-04-21 16:05:03 +0000

    added about page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get 'about' => 'welcome#about'
   
   get 'listings' => 'listings#index'
   get 'listings/new' => 'listings#new'
